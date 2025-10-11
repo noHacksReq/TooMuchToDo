6e65e7fefe35345304ca33da67cd747693e74d7d
Tor Rydberg <crownroyalsuperslut@yahoo.com>
2025-10-11 13:31:00 -0400

    added pinia store

--- project/src/main.ts
+++ project/src/main.ts
@@ -1,5 +1,13 @@
+//import { useTodoStore } from '/stores/todoStore'
+import { createPinia } from 'pinia'
+
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+const pinia = createPinia()
+const app = createApp(App)
+app.use(pinia)
+app.mount('#app')
 
-createApp(App).mount('#app')
+
+
