44f6d1535a0f5b450f26fabc0b35b0ac88ec119b
Tor Rydberg <crownroyalsuperslut@yahoo.com>
2025-10-14 12:44:10 -0400

    fixed bug updating object and pushing to array

--- project/stores/todoStore.ts
+++ project/stores/todoStore.ts
@@ -2,9 +2,9 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 export const useTodoStore = defineStore('todos', () => {
-    const test: String = ref('testing store');
-    let todoObj: Object = ref({title: ''});
+    
+    //let todoObj: Object = ref({title: ''});
     
     const todoArr: Array = ref([]);
-    return {todoObj, todoArr }
+    return { todoArr }
 })
