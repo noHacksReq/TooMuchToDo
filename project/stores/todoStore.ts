d98eddcafba388981f2a9f2e41bb7304d737fd7c
Tor Rydberg <crownroyalsuperslut@yahoo.com>
2025-10-15 09:00:33 -0400

    code cleanup

--- project/stores/todoStore.ts
+++ project/stores/todoStore.ts
@@ -2,9 +2,6 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 export const useTodoStore = defineStore('todos', () => {
-    
-    //let todoObj: Object = ref({title: ''});
-    
     const todoArr: Array = ref([]);
     return { todoArr }
 })
