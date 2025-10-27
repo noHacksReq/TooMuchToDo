b5a3bce9ce7fa8346b4d58ed9fa80e5823466b1a
Tor Rydberg <crownroyalsuperslut@yahoo.com>
2025-10-27 14:27:05 -0400

    added functionality to clear local storage

--- project/stores/todoStore.ts
+++ project/stores/todoStore.ts
@@ -2,14 +2,21 @@ import { defineStore } from 'pinia';
 import { ref, computed, reactive } from 'vue';
 
 export const useTodoStore = defineStore('todos', () => {
-    const todoArr: Array = ref([]);
+    let todoArr: Array = ref([]);
     
     // check local storage and set todoArr with its value
     if(localStorage.getItem("tempSavedArr")) {
         todoArr.value = JSON.parse(localStorage.getItem("tempSavedArr"))
-        
+    };
+
+       
+    function testAction() {
+        todoArr.value = [];
+        todoArr = localStorage.clear();
     }
     
-    return { todoArr }
+
+    return { todoArr, testAction }
 }
+    
 )
