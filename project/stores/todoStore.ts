6f9ca4f4b86ed15d206a828806bf1d8992d2caf6
Tor Rydberg <crownroyalsuperslut@yahoo.com>
2025-11-08 13:39:52 -0500

    added something

--- project/stores/todoStore.ts
+++ project/stores/todoStore.ts
@@ -3,34 +3,31 @@ import { ref, computed, reactive } from 'vue';
 
 export const useTodoStore = defineStore('todos', () => {
     let todoArr: Array = ref([]);
-    let canConfirm: Boolean = ref(false);
-    let clearModal: Boolean = ref(false);
     
     // check local storage and set todoArr with its value
     if(localStorage.getItem("tempSavedArr")) {
         todoArr.value = JSON.parse(localStorage.getItem("tempSavedArr"))
     };
 
-    function confirmClear() {
-       clearModal.value = true;
-    }
        
+
     function clearList() {
+        
         if(clearModal.value === true) {
             todoArr.value = localStorage.clear();
             todoArr.value = [];
+            debugger
             clearModal.value = false;
         }
     }
 
     function goBack() {
         clearModal.value = false;
+
     }
     
 
-    return { todoArr, clearList, 
-        canConfirm, clearModal, 
-        confirmClear, goBack  }
+    return { todoArr, testAction, goBack }
 }
     
 )
